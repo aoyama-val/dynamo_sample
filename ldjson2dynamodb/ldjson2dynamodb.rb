69c11252520de4bedcf226b7cd5c9335bffd1cb0
Aoyama, Shotaro <aoyama@val.co.jp>
2019-02-17 19:42:04 +0900

    mv

--- ldjson2dynamodb/ldjson2dynamodb.rb
+++ ldjson2dynamodb/ldjson2dynamodb.rb
@@ -3,16 +3,23 @@
 #   DynamoDBのテーブルにインポートする
 #
 #   ・エラーがあった場合はリトライせず、エラーファイルに書き出す
+#   ・この方式の良いところは、キャパシティ不足でエラーが起きてもエラーファイルを
+#   　食わせて再実行すれば全てのデータを漏れなくインポートできるところ。
+#
 ################################################################################
 
 require "aws-sdk"
 
 class Tsv2DynamoDB
+  attr_accessor :error_count
+
   def initialize(*args, **kwargs)
     @ddb = Aws::DynamoDB::Client.new(**kwargs)
   end
 
   def import(table_name, filename, error_filename)
+    @error_count = 0
+
     open(error_filename, "w") do |error_file|
       items = []
       linenum = 0
@@ -51,7 +58,41 @@ private
       unprocessed.each do |u|
         error_file.puts JSON.dump(u.put_request.item)
         error_file.flush
+        @error_count += 1
       end
     end
   end
+end
+
+if __FILE__ == $0
+  def main
+    if ARGV.length != 2
+      puts "Usage: ruby #{$0} TABLE_NAME FILENAME"
+      puts "Example: ruby #{$0} my_table sample.ldjson"
+      exit 1
+    end
+
+    table_name = ARGV[0]
+    filename = ARGV[1]
+    error_filename = "error_#{Time.now.strftime('%Y%m%d_%H%M%S')}.ldjson"
+
+    puts "Start"
+    puts "error_filename: #{error_filename}"
+
+    started = Time.now
+
+    td = Tsv2DynamoDB.new
+
+    begin
+      td.import(table_name, filename, error_filename)
+    rescue Interrupt
+      puts "Interrupted"
+    end
+
+    finished = Time.now
+    puts "Elapsed: #{finished - started} sec"
+    puts "error_filename: #{error_filename}"
+  end
+
+  main
 end
