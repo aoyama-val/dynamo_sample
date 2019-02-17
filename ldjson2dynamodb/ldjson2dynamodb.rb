5f8d8451ee5a1e55a71eddd55916331550f91a92
Aoyama, Shotaro <aoyama@val.co.jp>
2019-02-17 20:08:42 +0900

    キャパシティエラーが起きた場合はスリープして無限リトライするように

--- ldjson2dynamodb/ldjson2dynamodb.rb
+++ ldjson2dynamodb/ldjson2dynamodb.rb
@@ -10,12 +10,15 @@
 require "aws-sdk"
 
 class Tsv2DynamoDB
+  attr_accessor :capacity_error_count
+
   def initialize(*args, **kwargs)
     @ddb = Aws::DynamoDB::Client.new(**kwargs)
   end
 
   def import(table_name, filename, skip)
     @filename = filename
+    @capacity_error_count = 0
 
     items = []
     linenum = 0
@@ -45,17 +48,21 @@ private
         }
       }
     }
-    resp = @ddb.batch_write_item(
-      request_items: { table_name => requests },
-      return_consumed_capacity: "TOTAL",
-      return_item_collection_metrics: "SIZE"
-    )
-    unprocessed = resp.unprocessed_items[table_name]
-    puts "#{Time.now.strftime('%H:%M:%S')}   #{linenum - items.length + 1} - #{linenum}   consumed_capacity: #{resp.consumed_capacity[0].capacity_units}   #{unprocessed ? 'unprocessed: ' + unprocessed.length.to_s : ''}"
-    if unprocessed
-      puts "To retry:"
-      puts "  ruby #{$0} #{table_name} #{@filename} #{linenum - items.length}"
-      exit 1
+    loop do
+      resp = @ddb.batch_write_item(
+        request_items: { table_name => requests },
+        return_consumed_capacity: "TOTAL",
+        return_item_collection_metrics: "SIZE"
+      )
+      unprocessed = resp.unprocessed_items[table_name]
+      puts "#{Time.now.strftime('%H:%M:%S')}   #{linenum - items.length + 1} - #{linenum}   consumed_capacity: #{resp.consumed_capacity[0].capacity_units}   #{unprocessed ? 'unprocessed: ' + unprocessed.length.to_s : ''}"
+      if unprocessed
+        puts "書き込みキャパシティが不足しています！"
+        @capacity_error_count += 1
+        sleep 1
+      else
+        return
+      end
     end
   end
 end
@@ -63,8 +70,9 @@ end
 if __FILE__ == $0
   def main
     if ARGV.length < 2
-      puts "Usage: ruby #{$0} TABLE_NAME FILENAME SKIP"
-      puts "Example: ruby #{$0} my_table sample.ldjson 0"
+      puts "Usage: ruby #{$0} TABLE_NAME FILENAME [SKIP]"
+      puts "Example: ruby #{$0} my_table sample.ldjson 100"
+      puts "  SKIPを指定すると、入力ファイルの最初のSKIP行目をスキップします"
       exit 1
     end
 
@@ -86,6 +94,7 @@ if __FILE__ == $0
 
     finished = Time.now
     puts "Elapsed: #{finished - started} sec"
+    puts "書き込みキャパシティが不足した回数: #{td.capacity_error_count}"
   end
 
   main
